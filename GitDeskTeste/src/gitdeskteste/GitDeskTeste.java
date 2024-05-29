9f4993a42ee69f669b00a1276cb6b93513c8704d
Fabrício Carneiro <fabricioffcr7@gmail.com>
2024-05-29 15:50:37 -0300

    Hello world
    Adicionada função para retornar "Hello world"

--- GitDeskTeste/src/gitdeskteste/GitDeskTeste.java
+++ GitDeskTeste/src/gitdeskteste/GitDeskTeste.java
@@ -1,20 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package gitdeskteste;
 
-/**
- *
- * @author fabri
- */
-public class GitDeskTeste {
 
-    /**
-     * @param args the command line arguments
-     */
+public class GitDeskTeste {
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello world!");
     }
     
 }
